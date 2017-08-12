d26ac2ec67579762d06914cfaeca7eb7b0258446
James Lovejoy <jameslovejoy1@gmail.com>
2017-08-12 00:01:50 -0400

    Add VTC support

--- firmware/coins.c
+++ firmware/coins.c
@@ -35,6 +35,8 @@ const CoinType coins[COINS_COUNT] = {
 	{true, "Dash",          true, " DASH", true,   76, true,     100000, true,   16, true, "\x19" "DarkCoin Signed Message:\n", true, 0x02fe52cc, true, 0x02fe52f8, true, false, false, 0, },
 	{true, "Zcash",         true, " ZEC",  true, 7352, true,    1000000, true, 7357, true, "\x16" "Zcash Signed Message:\n",    true, 0x0488b21e, true, 0x0488ade4, true, false, false, 0, },
 	{true, "Zcash Testnet", true, " TAZ",  true, 7461, true,   10000000, true, 7354, true, "\x16" "Zcash Signed Message:\n",    true, 0x043587cf, true, 0x04358394, true, false, false, 0, },
+	{true, "Vertcoin",      true, " VTC",  true,   71, true,   10000000, true,    5, true, "\x18" "Vertcoin Signed Message:\n",  true, 0x0488b21e, true, 0x0488ade4, true, true,  false, 0, },
+	{true, "Vertcoin Testnet", true, " TVTC", true,   74, true,   10000000, true,  196, true, "\x18" "Vertcoin Signed Message:\n",  true, 0x043587cf, true, 0x04358394, true, true,  false, 0, },
 };
 
 const CoinType *coinByName(const char *name)
